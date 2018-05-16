b48b9cfc878f0a65cef127438f374d8f72711de0
floria1998 <floria1998@163.com>
2018-05-16 18:51:47 +0800

    Add files via upload

--- Lab6/ConvertToDecimal.vhd
+++ Lab6/ConvertToDecimal.vhd
@@ -4,19 +4,22 @@ use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
 use work.my_data_types.all;
 
+
 entity ConvertToDecimal is
 port(
     Data: in std_logic_vector(15 downto 0);
 	clk: in std_logic;
-	DataOut: out rational_number(1 downto 0)
+	DataOut: out rational_number(4 downto 0)
 	--DataOut_integer: out integer;
 	--DataOut_decimal: out integer;
 	);
 end entity;
 
 architecture behavior of ConvertToDecimal is
+
+
 ------signal declaration
-signal tmp: integer :=0;
+--signal tmp: integer :=0;
 signal DataOut_integer: integer:=0;
 signal DataOut_decimal: integer:=0;
 
@@ -44,7 +47,10 @@ signal Data8: std_logic;
 signal Data9: std_logic;
 signal Data10: std_logic;
 
---signal Data1: integer :=0;
+signal da,db:std_logic_vector(3 downto 0);
+signal d1,d2,d3,d4:std_logic_vector(3 downto 0);
+
+
 
 ----end signal declaration
 
@@ -80,19 +86,81 @@ begin
 	  Data10<=Data(10);
 	  Data10_1<=conv_integer(Data10);
 
-      tmp <=((Data10_1)*1024+(Data9_1)*512+(Data8_1)*256+(Data7_1)*128+(Data6_1)*64+Data5_1*32+Data4_1*16+Data3_1*8+Data2_1*4+Data1_1*2+Data0_1);
+     -- tmp <=((Data10_1)*1024+(Data9_1)*512+(Data8_1)*256+(Data7_1)*128+(Data6_1)*64+Data5_1*32+Data4_1*16+Data3_1*8+Data2_1*4+Data1_1*2+Data0_1);
       DataOut_integer<=(Data10_1*64+Data9_1*32+Data8_1*16+Data7_1*8+Data6_1*4+Data5_1*2+Data4_1);
 	  DataOut_decimal<=(Data3_1*5000+Data2_1*2500+Data1_1*1250+Data0_1*625);
     end if;
 end process;
 
-----dataout
+----convert DataOut_integer to DataOut(4)&DataOut(3) in std_logic_vector form
 process(clk)
+variable tmp: integer range 0 to 99:=0;
 begin
     if(clk'event and clk='1') then
-	  DataOut(1)<=DataOut_integer;
-	  DataOut(0)<=DataOut_decimal;
-	 end if;
+	    if(tmp<DataOut_integer) then
+		    if(da=9 and db=9) then
+			    da<="0000"; 
+				db<="0000";
+				tmp:=0;
+			elsif(da=9) then
+				    da<="0000";
+					db<=db+1;
+					tmp:=tmp+1;
+			else da<=da+1;
+			     tmp:=tmp+1;
+			end if;
+		else 
+		     DataOut(4)<=db;
+		     DataOut(3)<=da;
+		     da<="0000";
+		     db<="0000";
+			 tmp:=0;
+		end if;
+	end if;
 end process;
 
+---connvert DataOut_decimal to DataOut(2),DataOut(1)&DataOut(0) in std_logic_vector form
+process(clk)
+variable tmp1: integer range 0 to 9999:=0;
+begin
+    if(clk'event and clk='1') then
+	    if(tmp1<DataOut_decimal) then
+		    if(d1=9 and d2=9 and d3=9 and d4=9) then
+			    d1<="0000";
+				d2<="0000";
+				d3<="0000";
+				d4<="0000";
+				tmp1:=0;
+			elsif(d1=9 and d2=9 and d3=9) then
+			    d1<="0000";
+				d2<="0000";
+				d3<="0000";
+				d4<=d4+1;
+				tmp1:=tmp1+1;
+			elsif(d1=9 and d2=9) then
+			    d1<="0000";
+				d2<="0000";
+				d3<=d3+1;
+				tmp1:=tmp1+1;
+			elsif(d1=9) then
+			    d1<="0000";
+			    d2<=d2+1;
+				tmp1:=tmp1+1;
+			else
+			    d1<=d1+1;
+				tmp1:=tmp1+1;
+			end if;
+		else
+		    tmp1:=0;
+			DataOut(2)<=d4;
+			DataOut(1)<=d3;
+			DataOut(0)<=d2;
+			d1<="0000";
+			d2<="0000";
+			d3<="0000";
+			d4<="0000";
+		end if;
+	end if;
+end process;
+	  
 end behavior;
--- Lab6/work.vhd
+++ Lab6/work.vhd
@@ -3,5 +3,5 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 package my_data_types is
-    type rational_number is array( natural range<>) of integer;
+    type rational_number is array( natural range<>) of std_logic_vector(3 downto 0);
 end my_data_types;
